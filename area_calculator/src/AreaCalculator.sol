7d1007d3445bb02ea8d70aaeb8dfe6d638afa86c
rozzy <rosekaremeri@gmail.com>
2024-08-22 21:18:10 +0300

    Function to calculate triange

--- area_calculator/src/AreaCalculator.sol
+++ area_calculator/src/AreaCalculator.sol
@@ -3,5 +3,10 @@ pragma solidity ^0.8.0;
 
 contract AreaCalculator {
     
-  
+    // Function to calculate the area of a triangle
+    function calculateTriangleArea(uint256 base, uint256 height) public pure returns (uint256) {
+        require(base > 0 && height > 0, "Base and height must be greater than zero");
+        return (base * height) / 2;
+    }
+
 }
