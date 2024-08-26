3d3626f27489133fdff0f3f49ff05c4b77cf8bb4
rozzy <rosekaremeri@gmail.com>
2024-08-26 22:42:44 +0300

    todo struct

--- smart_contracts_week/contracts/Todo.sol
+++ smart_contracts_week/contracts/Todo.sol
@@ -2,5 +2,27 @@
 pragma solidity ^0.8.7;
 
 contract Todo {
-    
+    struct TodoItem {
+        string title;
+        string description;
+        bool isDone;
+    }
+
+    TodoItem[] todos;
+
+    function createTodo(
+        string memory _title,
+        string memory _description
+    ) external {
+//initialize a struct
+
+
+    }
+
+    function getTodos() external view returns (TodoItem[] memory) {}
+
+    function getTodo(uint256 _index) external view returns (TodoItem memory) {}
+
+    //updating notes
+    function updateTodo(uint256 _index) external {}
 }
