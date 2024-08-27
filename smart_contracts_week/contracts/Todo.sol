b2bf1603543d55afb255cb453580875c9e629290
rozzy <rosekaremeri@gmail.com>
2024-08-27 19:19:19 +0300

    setter and getter functions, that works

--- smart_contracts_week/contracts/Todo.sol
+++ smart_contracts_week/contracts/Todo.sol
@@ -14,14 +14,23 @@ contract Todo {
         string memory _title,
         string memory _description
     ) external {
-//initialize a struct
+        //initialize a struct
+        //default value of bool is false
+        todos.push(TodoItem(_title, _description, false));
+
+        //2nd way
 
 
     }
 
-    function getTodos() external view returns (TodoItem[] memory) {}
+    function getTodos() external view returns (TodoItem[] memory) {
+        return todos;
+    }
 
-    function getTodo(uint256 _index) external view returns (TodoItem memory) {}
+    function getTodo(uint256 _index) external view returns (TodoItem memory) {
+        
+        return todos[_index];
+    }
 
     //updating notes
     function updateTodo(uint256 _index) external {}
