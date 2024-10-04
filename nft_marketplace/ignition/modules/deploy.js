045476ef993471fd4fe12bee827f224c82b4b5e3
rozzy <rosekaremeri@gmail.com>
2024-10-04 12:58:37 +0300

    deploy

--- nft_marketplace/ignition/modules/deploy.js
+++ nft_marketplace/ignition/modules/deploy.js
@@ -1,15 +1,24 @@
-import { ethers } from 'hardhat';
+const { ethers } = require("hardhat");
 
 async function main() {
-    // Deploy the NFT Marketplace contract
-    const NftMarket = await ethers.getContractFactory('NftMarket'); // Use getContractFactory to get the contract
-    const nftMarket = await NftMarket.deploy(); // Deploy the contract
-    await nftMarket.deployed(); // Wait for deployment to finish
+  // Deploy the NFT Marketplace contract
+  const nftMarket = await ethers.deployContract("NftMarket"); // Deploy the contract
 
-    console.log('NFTMarket Contract Deployed at: ' + nftMarket.address); // Log the deployed address
+  // Wait for deployment (optional, depending on your setup)
+  await nftMarket.waitForDeployment();
+
+  console.log("NFTMarket Contract Deployed at: " + nftMarket.target); // Log the deployed address
+
+  // Deploy the NFT  contract
+  const myNFT = await ethers.deployContract("MyNFT"); // Deploy the contract
+
+  // Wait for deployment (optional, depending on your setup)
+  await myNFT.waitForDeployment();
+
+  console.log("MyNFT Contract Deployed at: " + myNFT.target); // Log the deployed address
 }
 
 main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
+  console.error(error);
+  process.exitCode = 1; // Exit with failure code
 });
